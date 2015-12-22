97aa765ff06666fcd5c5e7987ea9da97b9a96d6c
Dylan Klett <dylanzklett@gmail.com>
2015-12-22 14:56:26 -0500

    added Jon's checkout changes

--- main.rb
+++ main.rb
@@ -7,6 +7,15 @@ get '/light_side' do
 	erb :light_side
 end
 
+post '/light_side' do
+	puts params.inspect
+	erb :checkout
+end
+
 get '/dark_side' do
 	erb :dark_side
+end
+
+get '/checkout' do 
+	erb :checkout
 end
